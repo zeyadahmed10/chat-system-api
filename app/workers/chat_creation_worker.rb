ea710104e7eb69776cadce3cdc1b01f9a1deb80e
Zeyad Ahmed <zeyadfahem1@gmail.com>
2024-09-28 14:06:18 +0300

    adding retry functionality to the wokrers, incase of db-connectivity issue or anything went wrong

--- app/workers/chat_creation_worker.rb
+++ app/workers/chat_creation_worker.rb
@@ -1,15 +1,34 @@
-class ChatCreationWorker
-    include Sidekiq::Worker
+# class ChatCreationWorker
+#     include Sidekiq::Worker
     
-    sidekiq_options queue: 'chats'
+#     sidekiq_options queue: 'chats'
 
-    def perform(application_token, chat_number)
+#     def perform(application_token, chat_number)
+#       chat = Chat.new(application_token: application_token, chat_number: chat_number)  
+#       if chat.save
+#         Rails.logger.info "Chat created successfully: #{chat.chat_number}"
+#       else
+#         Rails.logger.error "Failed to create chat: #{chat.errors.full_messages.join(', ')}"
+#       end
+#     end
+#   end
+class ChatCreationWorker
+  include Sidekiq::Worker
+  
+  sidekiq_options queue: 'chats', retry: 5  # retry with exponential backoff
+
+  def perform(application_token, chat_number)
+    Chat.transaction do
       chat = Chat.new(application_token: application_token, chat_number: chat_number)  
       if chat.save
         Rails.logger.info "Chat created successfully: #{chat.chat_number}"
       else
         Rails.logger.error "Failed to create chat: #{chat.errors.full_messages.join(', ')}"
+        raise ActiveRecord::Rollback
       end
     end
   end
+
+end
+
   
--- app/workers/update_counts_worker.rb
+++ app/workers/update_counts_worker.rb
@@ -4,8 +4,10 @@ class UpdateCountsWorker
     sidekiq_options queue: 'default'
   
     def perform
-      update_chats_count
-      update_messages_count
+      ActiveRecord::Base.transaction do
+        update_chats_count
+        update_messages_count
+      end
     end
   
     private
@@ -13,14 +15,19 @@ class UpdateCountsWorker
     def update_chats_count
       Application.find_each do |application|
         chats_count = Chat.where(application_token: application.application_token).count
-        application.update(chats_count: chats_count)
+        unless application.update(chats_count: chats_count)
+          raise ActiveRecord::Rollback 
+        end
       end
     end
   
+
     def update_messages_count
       Chat.find_each do |chat|
         messages_count = Message.where(application_token: chat.application_token, chat_number: chat.chat_number).count
-        chat.update(messages_count: messages_count)
+        unless chat.update(messages_count: messages_count)
+          raise ActiveRecord::Rollback
+        end
       end
     end
   end
